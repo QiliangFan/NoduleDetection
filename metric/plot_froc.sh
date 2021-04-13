66a9cc4400ceceb1aeb41b711bb36420b4d8554f
liang <1035847702@qq.com>
2021-04-13 02:40:10 -0400

    modify_in_210

--- metric/plot_froc.sh
+++ metric/plot_froc.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python3 froc.py /home/maling/fanqiliang/projects/NoduleDetection/test/sub_first/output.csv
+# python3 froc.py /home/maling/fanqiliang/projects/NoduleDetection/test/sub_first/output.csv
+
+python3 froc.py /home/fanrui/fanqiliang/projects/NoduleDetection/detect_test/output.txt
